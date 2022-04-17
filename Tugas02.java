784e543cad750f47144b550fe32cef5979d44102
20101072 <101173886+20101072@users.noreply.github.com>
2022-04-16 22:40:42 -0700

    Add files via upload

---
+++ Tugas02.java
@@ -0,0 +1,28 @@
+[04.29, 16/4/2022] Cahya: Ngae tugas oop ci jak dwil ?
+[04.31, 16/4/2022] Kristinong: aw ca
+[04.31, 16/4/2022] Cahya: Nahhh , w ngantiang koding gn be neh wkwkwk
+[04.34, 16/4/2022] Kristinong: wkwkwk nhnh
+[07.43, 16/4/2022] Kristinong: package tugas.pkg2;
+
+/**
+ *
+ * @author CAHYA KURNIA 20101072
+ */
+public class Tugas2 {
+   
+    public static void main(String[] args) {
+         /*membuat object  segitiga dari class LuasSegitiga
+        */
+        volumebalok balok = new volumebalok(7,5,4);
+        
+        //set nilai ke property
+        //balok.setPJ(7);
+        //balok.setLb(5);
+        //balok.setTg(4);
+        
+        //menampilkan hasil
+        balok.CetakVolumeBalok();
+    }
+        
+    
+}
